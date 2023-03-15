e3192647caed64dabdf55a6784fab6cf28856014
gjf2a <ferrer@hendrix.edu>
2023-03-15 09:00:34 -0500

    Added bombs!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 #![cfg_attr(not(test), no_std)]
 
-use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, is_drawable};
+use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, plot_num, plot_str};
 use pc_keyboard::{DecodedKey, KeyCode};
+use rand::SeedableRng;
+use rand::rngs::SmallRng;
+use rand::RngCore;
+use core::default::Default;
+
+const NEW_WALL_FREQ: isize = 100;
+const NEW_BOMB_FREQ: isize = 20;
 
 const WALLS: &str = "################################################################################
 #                                                                              #
@@ -31,12 +38,18 @@ const WALLS: &str = "###########################################################
 
 pub struct Game {
     player: Player,
-    walls: Walls,
+    walls: Items,
+    explosives: Items,
+    tick_count: isize,
+    // Obtained from: https://stackoverflow.com/questions/67627335/how-do-i-use-the-rand-crate-without-the-standard-library
+    rng: SmallRng,
 }
 
 impl Game {
     pub fn new() -> Self {
-        Self {player: Player::new(), walls: Walls::new(WALLS)}
+        let mut explosives = Items::default();
+        explosives.change_color(Color::LightRed);
+        Self {player: Player::new(), explosives, walls: Items::new(WALLS, Color::LightGreen), tick_count: 0, rng: SmallRng::seed_from_u64(3)}
     }
 
     pub fn key(&mut self, key: DecodedKey) {
@@ -59,7 +72,12 @@ impl Game {
                     _ => {}
                 }
                 if !future.is_colliding(&self.walls) {
+                    plot(' ', self.player.x, self.player.y, ColorCode::new(Color::Black, Color::Black));
                     self.player = future;
+                }
+                if self.player.is_colliding(&self.explosives) {
+                    self.player.bomb_count += 1;
+                    self.explosives.remove(self.player.y, self.player.x);
                 }
             }
             DecodedKey::Unicode(_) => {}
@@ -67,40 +85,78 @@ impl Game {
     }
 
     pub fn tick(&mut self) {
+        self.tick_count += 1;
+        if self.tick_count % NEW_WALL_FREQ == 0 {
+            self.walls.add_random_item(&mut self.rng);
+        }
+        if self.tick_count % NEW_BOMB_FREQ == 0 {
+            self.explosives.add_random_item(&mut self.rng);
+        }
         self.walls.draw();
+        self.explosives.draw();
         plot('*', self.player.x, self.player.y, ColorCode::new(Color::Green, Color::Black));
+        plot_num(self.tick_count, BUFFER_WIDTH / 2, 0, ColorCode::new(Color::LightGray, Color::Black));
+        plot_str("Bombs:", 60, 0, ColorCode::new(Color::LightRed, Color::Black));
+        plot_num(self.player.bomb_count as isize, 66, 0, ColorCode::new(Color::LightRed, Color::Black));
     }
 }
 
-pub struct Walls {
-    walls: [[bool; BUFFER_WIDTH]; BUFFER_HEIGHT]
+pub struct Items {
+    items: [[bool; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    color: Color,
 }
 
-impl Walls {
-    pub fn new(map: &str) -> Self {
+impl Default for Items {
+    fn default() -> Self {
+        Self { items: [[false; BUFFER_WIDTH]; BUFFER_HEIGHT], color: Color::White}
+    }
+}
+
+impl Items {
+    pub fn new(map: &str, color: Color) -> Self {
         let mut walls = [[false; BUFFER_WIDTH]; BUFFER_HEIGHT];
         for (row, chars) in map.split('\n').enumerate() {
             for (col, value) in chars.char_indices() {
                 walls[row][col] = value == '#';
             }
         }
-        Self {walls}
+        Self {items: walls, color}
+    }
+
+    pub fn add_random_item(&mut self, rng: &mut SmallRng) {
+        let col: usize = 1 + rng.next_u32() as usize % (BUFFER_WIDTH - 1);
+        let row: usize = 1 + rng.next_u32() as usize % (BUFFER_HEIGHT - 1);
+        self.add(row, col);
+    }
+
+    pub fn change_color(&mut self, color: Color) {
+        self.color = color;
     }
 
     pub fn draw(&self) {
-        for row in 0..self.walls.len() {
-            for col in 0..self.walls[row].len() {
-                plot(self.char_at(row, col), col, row, ColorCode::new(Color::Blue, Color::Black));
+        for row in 0..self.items.len() {
+            for col in 0..self.items[row].len() {
+                if self.occupied(row, col) {
+                    plot(self.char_at(row, col), col, row, ColorCode::new(self.color, Color::Black));
+                }
             }
         }
     }
 
     pub fn occupied(&self, row: usize, col: usize) -> bool {
-        self.walls[row][col]
+        self.items[row][col]
+    }
+
+    pub fn add(&mut self, row: usize, col: usize) {
+        self.items[row][col] = true;
+    }
+
+    pub fn remove(&mut self, row: usize, col: usize) {
+        self.items[row][col] = false;
     }
 
     fn char_at(&self, row: usize, col: usize) -> char {
-        if self.walls[row][col] {
+        if self.items[row][col] {
             '#'
         } else {
             ' '
@@ -112,14 +168,15 @@ impl Walls {
 pub struct Player {
     x: usize,
     y: usize,
+    bomb_count: usize,
 }
 
 impl Player {
     pub fn new() -> Self {
-        Self {x: BUFFER_WIDTH / 2, y: BUFFER_HEIGHT / 2}
+        Self {x: BUFFER_WIDTH / 2, y: BUFFER_HEIGHT / 2, bomb_count: 0}
     }
 
-    pub fn is_colliding(&self, walls: &Walls) -> bool {
+    pub fn is_colliding(&self, walls: &Items) -> bool {
         walls.occupied(self.y, self.x)
     }
 
